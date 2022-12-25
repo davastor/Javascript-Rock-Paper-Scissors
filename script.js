706126dc66a5b896c6b99194f29d9929c6bc0999
Danh Dang <danhastor@gmail.com>
2022-12-25 15:21:02 -0800

    Add gameOver and resetGame

--- script.js
+++ script.js
@@ -7,14 +7,25 @@ let round = document.getElementById("round");
 let playerWins = document.getElementById("playerWins");
 let computerWins = document.getElementById("computerWins");
 let ties = document.getElementById("ties");
+let gameOver = document.getElementById("game-over");
+let container = document.querySelector(".container");
 let roundCount = 0;
 let winCount = 0;
 let lossCount = 0;
 let tieCount = 0;
+let resetButton;
 
-rockBtn.addEventListener("click", (event) => {game('rock');});
-paperBtn.addEventListener("click", (event) => {game('paper');});
-scissBtn.addEventListener("click", (event) => {game('scissors');});
+rockBtn.addEventListener("click", () => { game('rock');});
+paperBtn.addEventListener("click", () => {game('paper');});
+scissBtn.addEventListener("click", () => {game('scissors');});
+
+
+function checkEnd(){
+    if (roundCount == 5){
+        endGame();
+    }
+}
+
 
 function getComputerChoice(){
     let choices = ['rock', 'paper', 'scissors'];
@@ -45,10 +56,13 @@ function playOneRound(playerChoice, computerChoice){
     else{
         return 'tie';
     }
+
+
     
 }
 
 function game(playerChoice){
+
     let computerChoice = getComputerChoice();
     let roundResult = playOneRound(playerChoice, computerChoice);
     roundCount++;
@@ -56,30 +70,76 @@ function game(playerChoice){
     playerChoiceText.textContent = `You chose ${playerChoice}.`;
     result.textContent = `The computer chose ${computerChoice}.`;
 
+    updateScore(roundResult);
+
+    round.textContent = `Round: ${roundCount}`;
+    playerWins.textContent = `Player Wins: ${winCount}`;
+    computerWins.textContent = `Computer Wins: ${lossCount}`;
+    ties.textContent = `Ties: ${tieCount}`;
+    checkEnd();
+   
+}
+
+function updateScore(roundResult){
+
     if(roundResult == 'won'){
         result.style.backgroundColor = 'green';
         result.textContent += '  You win this round!';
         winCount++;
+
 
     }
     else if(roundResult == 'lost'){
         result.style.backgroundColor = 'red';
         result.textContent += '  You lost this round!';
         lossCount++;
+
     }
     else{
         result.style.backgroundColor = 'yellow';
         result.textContent += '  This round is a tie!';
         tieCount++;
     }
-    round.textContent = `Round: ${roundCount}`;
-    playerWins.textContent = `Player Wins: ${winCount}`;
-    computerWins.textContent = `Computer Wins: ${lossCount}`;
-    ties.textContent = `Ties: ${tieCount}`;
-        
+}
 
+function endGame(){
+    if(winCount > lossCount){
+        gameOver.textContent = 'Game over! You win!';
+        gameOver.style.backgroundColor = 'green';
+    }
+    else if (lossCount > winCount){
+        gameOver.textContent = 'Game over! You lose!';
+        gameOver.style.backgroundColor = 'red';
+    }
+    else{
+        gameOver.textContent = "Game over! It's a tie!";
+        gameOver.style.backgroundColor = 'yellow';
+    }
 
+    rockBtn.disabled = true;
+    scissBtn.disabled = true;
+    paperBtn.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start Over';
+    resetButton.style.margin = "4px";
+    container.appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame);
+}
 
-    
-    
+function resetGame(){
+    rockBtn.disabled = false;
+    scissBtn.disabled = false;
+    paperBtn.disabled = false;
+    roundCount = 0;
+    winCount = 0;
+    lossCount = 0;
+    tieCount = 0;
+    playerChoiceText.textContent = '';
+    result.textContent = '';
+    round.textContent = '';
+    ties.textContent = '';
+    playerWins.textContent = '';
+    computerWins.textContent = '';
+    gameOver.textContent = '';
+    container.removeChild(resetButton);
 }
